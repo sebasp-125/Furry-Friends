aa2206f8baecc86ab6753b8982bfbcd333c41e40
sebasp-125 <sp6027193@gmail.com>
2024-05-28 15:11:40 -0600

    90%

--- furry-friends/src/Components/LandingPage.js
+++ furry-friends/src/Components/LandingPage.js
@@ -1,17 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import '../CSS Globlal/css.css';
 
 export default function LandingPage() {
-    const [colorPets, set_ColorPets] = useState(false);
+    const [colorPets, setColorPets] = useState();
+    const [caninos, setCaninos] = useState([]);
+    const [felinos, setFelinos] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://adopcion-alpha.vercel.app/perros')
+            .then((response) => {
+                console.log("Los Datos Caninos han sido recibidos correctamente");
+                setCaninos(response.data);
+            })
+            .catch((error) => {
+                console.error("Error al recibir los datos caninos:", error);
+            });
+    }, []);
+
+    useEffect(() => {
+        axios.get('https://adopcion-alpha.vercel.app/gatos')
+            .then((response) => {
+                console.log("Los datos Felinos han sido recibidos correctamente");
+                setFelinos(response.data);
+            })
+            .catch((error) => {
+                console.error("Error al recibir los datos felinos:", error);
+            });
+    }, []);
 
     const handleOcolorPetsTrue = (event) => {
         event.preventDefault();
-        set_ColorPets(true);
+        setColorPets(true);
     };
 
     const handleOcolorPetsFalse = (event) => {
         event.preventDefault();
-        set_ColorPets(false);
+        setColorPets(false);
     };
 
     return (
@@ -28,12 +53,38 @@ export default function LandingPage() {
                     <img className="logo_gato" src="https://res.cloudinary.com/dbwgsrqgm/image/upload/v1702604543/Adopcion/perros/perros_oetwuw.png" alt="" />
                     <p className="nombre_g_p">Perros</p>
                 </div>
-                <div className={colorPets ? "content_gatos " : "content_gatos active"} onClick={handleOcolorPetsFalse}>
+                <div className={colorPets ? "content_gatos" : "content_gatos active"} onClick={handleOcolorPetsFalse}>
                     <img src="https://res.cloudinary.com/dbwgsrqgm/image/upload/v1702604464/Adopcion/gatos/Ellipse_8_pzg5km.png" alt="" className="background_img" />
                     <img className="logo_gato" src="https://res.cloudinary.com/dbwgsrqgm/image/upload/v1702604475/Adopcion/gatos/gatos_lgx2mu.png" alt="" />
                     <p className="nombre_g_p">Gatos</p>
                 </div>
+           
             </div>
+            <div className='content-pets'>
+                    {colorPets && (
+                        <div className='caninos-content'>
+                            {caninos.map((revisecanine , index) => (
+                                <div key={index}>
+                                    <img src={revisecanine.url} alt={`Imagen del Canino ${revisecanine.nombre}`} />
+                                    <p>Nombre: {revisecanine.nombre}</p>
+                                    <p>Raza: {revisecanine.raza}</p>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                    {!colorPets && (
+                        <div className='felinos-content'>
+                            {felinos.map((revisefelines , index) => (
+                                <div key={index}>
+                                    <img src={revisefelines.url} alt={`Imagen del Felino ${revisefelines.nombre}`} />
+                                    <p>Nombre: {revisefelines.nombre}</p>
+                                    <p>Edad: {revisefelines.edad}</p>
+                                    <p>Raza: {revisefelines.raza}</p>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
         </div>
     );
 }
